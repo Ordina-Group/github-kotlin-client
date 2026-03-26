f1eb8c0c00fb6dd8be74f0651b3631988e277fb3
Donovan <donovan.dekuiper@soprasteria.com>
2026-03-26 14:04:23 +0100

    Fix issues with determining version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
-    id("org.jreleaser") version "1.7.0"
+    id("org.jreleaser") version "1.18.0"
     id("pl.allegro.tech.build.axion-release") version "1.18.13"
     id("org.jetbrains.dokka") version "2.0.0"
     `java-library`
